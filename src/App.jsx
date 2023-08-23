73a5d3679240c338b18f1c77523d5f8d9292f9ee
Michael Hamilton <michael.ts.hamilton@gmail.com>
2023-08-23 17:16:53 -0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Memory Card</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
